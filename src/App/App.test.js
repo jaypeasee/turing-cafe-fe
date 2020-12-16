663c46d1e294bbec7f1f88fc0f747fc857739181
JP Carey <jpcarey4@gmail.com>
2020-12-16 10:35:46 -0700

    test that the id and name are in the document after the form is submitted

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 describe("App", () => {
-  
+  it('should have a new reservation show on the page after creating one in the form', () => {
+    render(<App />)
+
+    const nameInput = screen.getByPlaceholderText("Name")
+    const dateInput = screen.getByPlaceholderText("Date")
+    const timeInput = screen.getByPlaceholderText("Time")
+    const numberInput = screen.getByPlaceholderText("Number of Guests")
+    const submitBtn = screen.getByText("Make Reservation")
+
+    Date.now = jest.fn().mockImplementation(() => 666)
+    userEvent.type(nameInput, "Remus Lupin")
+    userEvent.type(dateInput, "10/31")
+    userEvent.type(timeInput, "6:30")
+    userEvent.type(numberInput, "1")
+        
+    userEvent.click(submitBtn)
+
+    expect(screen.getByTestId("666")).toBeInTheDocument()
+    expect(screen.getByText("Remus Lupin")).toBeInTheDocument()
+
+  })
 })
