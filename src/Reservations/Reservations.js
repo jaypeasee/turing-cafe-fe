9e2f1278f6f3ef4d8d7bbeff3f9b95e3209c0c08
JP Carey <jpcarey4@gmail.com>
2020-12-16 09:26:11 -0700

    fix declaration, return typos in Reservations component

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './Reservations.css'
-import reservationCards from '../ReservationCard/ReservationCard'
+import ReservationCard from '../ReservationCard/ReservationCard'
 
 const Reservations = (props) => {
     const reservationCards = props.reservations.map(resy => {
-        <ReservationCard 
-            id={resy.id}
-            name={resy.name}
-            date={resy.date}
-            time={resy.time}
-            number={resy.number}
-        />
+        return (
+            <ReservationCard 
+                id={resy.id}
+                name={resy.name}
+                date={resy.date}
+                time={resy.time}
+                number={resy.number}
+            />
+        )
     })
     return (
         <section className="resy-card">
-            {reservationCard}
+            {reservationCards}
         </section>
     )
 }
