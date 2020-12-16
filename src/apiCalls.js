df483b4b12ae02e2ddae6dd33a6417945ed6fec7
JP Carey <jpcarey4@gmail.com>
2020-12-16 10:42:34 -0700

    successfully post new reservations to the api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,14 @@ const getAllReservations = () => {
 }
 
 const postNewReservation = (newReservation) => {
-    return fetch('http://localhost:3001/api/v1/reservations')
+    return fetch('http://localhost:3001/api/v1/reservations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newReservation)
+    })
+        .then(response => response.json)
     
 }
 
