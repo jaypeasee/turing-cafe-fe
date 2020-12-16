d6f88ff3157480c6d11f3a077ee14cb2ac87e2b2
JP Carey <jpcarey4@gmail.com>
2020-12-16 09:23:47 -0700

    make reservation card component

---
+++ src/ReservationCard/ReservationCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './ReservationCard.css'
+
+const ReservationCard = (props) => {
+    const {id, name, date, time, number} = props
+    return (
+        <section
+            className='resy-card'
+            data-testid={id}
+        >
+            <h2>{name}</h2>
+            <p>{date}</p>
+            <p>{time}</p>
+            <p>{number}</p>
+        </section>
+    )
+}
+
+export default ReservationCard
