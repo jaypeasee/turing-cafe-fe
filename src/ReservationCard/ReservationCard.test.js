741b7391a2ca993841e2b520ea50b4bb9a6f06ee
JP Carey <jpcarey4@gmail.com>
2020-12-16 10:16:55 -0700

    setup reservation card test with imports and describe block

---
+++ src/ReservationCard/ReservationCard.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReservationCard from './ReservationCard'
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
+
+describe('ReservationForm', () => {
+    
+})
