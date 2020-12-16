d0da6ec50eed4bb940266a1cc870bfc1f9af4a6f
JP Carey <jpcarey4@gmail.com>
2020-12-16 10:30:52 -0700

    set up test for app with imports and describe block

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@testing-library/jest-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("App", () => {
+  
+})
