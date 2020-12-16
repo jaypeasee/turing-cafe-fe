e1eeeb50123d9b1bcbad5ac12b29c6e9c6a11375
JP Carey <jpcarey4@gmail.com>
2020-12-16 10:23:14 -0700

    test that all reservation card elements and props are in the document

--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -4,5 +4,22 @@ import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
 
 describe('ReservationForm', () => {
-    
+    it('should have an id, name, date, time, number, and cancel button', () => {
+        render(
+            <ReservationCard
+                id={3}
+                name="Ron Weasley"
+                date="12/25"
+                time="10:00"
+                number={7}
+            />
+        )
+        
+        expect(screen.getByTestId("3")).toBeInTheDocument()
+        expect(screen.getByText("Ron Weasley")).toBeInTheDocument()
+        expect(screen.getByText("12/25")).toBeInTheDocument()
+        expect(screen.getByText("10:00")).toBeInTheDocument()
+        expect(screen.getByText(7)).toBeInTheDocument()
+        expect(screen.getByText("Cancel")).toBeInTheDocument()
+    })
 })
