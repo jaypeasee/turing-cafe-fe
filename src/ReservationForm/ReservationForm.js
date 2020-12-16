4a2d6ea43d36bbfa6fe343603c2e769dd3db20f0
JP Carey <jpcarey4@gmail.com>
2020-12-16 09:49:54 -0700

    build out jsx reservation form

--- src/ReservationForm/ReservationForm.js
+++ src/ReservationForm/ReservationForm.js
@@ -5,10 +5,51 @@ class ReservationForm extends Component {
     constructor() {
         super()
         this.state = {
-
+            name: "",
+            date: "",
+            time: "",
+            number: ""
         }
     }
+
+    updateInput = (event) => {
+
+    }
+
     render() {
-        
+        return (
+            <form className="resy-form">
+            <input 
+                className="resy-form-input"
+                name="name"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.updateInput}
+            />
+            <input 
+                className="resy-form-input"
+                name="date"
+                placeholder="Date"
+                value={this.state.date}
+                onChange={this.updateInput}
+            />
+            <input 
+                className="resy-form-input"
+                name="time"
+                placeholder="Time"
+                value={this.state.time}
+                onChange={this.updateInput}
+            />
+            <input 
+                className="resy-form-input"
+                name="number"
+                placeholder="Number of Guests"
+                value={this.state.number}
+                onChange={this.updateInput}
+            />
+        </form>
+        )
     }
 }
+
+export default ReservationForm
